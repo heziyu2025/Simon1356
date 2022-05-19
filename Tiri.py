fded85969eb87ce0f62b5a045a090b65345c38a0
Yuanhui <hyh618@gmail.com>
2022-05-19 11:22:36 +0800

    优化了帮助界面

--- Tiri.py
+++ Tiri.py
@@ -2,6 +2,11 @@ import tkinter as tk
 from tkinter import ttk
 
 class applacation():
+    def helpQuit(self):
+        print('Quit')
+        self.helpRoot.destroy()
+        self.root.mainloop()
+
     def send(self, key=None):
         if self.mainSendText.get() != '':
             self.mainDialog.configure(state=tk.NORMAL)
@@ -13,8 +18,14 @@ class applacation():
     def help(self):
         self.helpRoot = tk.Tk()
         self.helpRoot.title('帮助')
-        tk.Label(self.helpRoot,
-                 text="算数题：打出”算术题“，加空格，再写要计算算式，\n讲笑话：输入“讲笑话”\n退出：打出“quit”\n打出“节日快乐”会有惊喜").pack()
+        self.helpRoot.geometry('300x120')
+        self.helpText = tk.Text(self.helpRoot, width=35, height=5, font=('宋体', 12))
+        self.helpText.insert(1.0, "算数题：打出”算术题“，加空格，再写要计算算式\n讲笑话：输入“讲笑话”\n退出：打出“quit”\n打出“节日快乐”会有惊喜")
+        self.helpText.configure(state=tk.DISABLED)
+        self.helpText.pack()
+        self.helpButton = ttk.Button(self.helpRoot, text='知道了', command=self.helpQuit)
+        self.helpButton.pack()
+        self.helpRoot.mainloop()
 
     def __init__(self):
         self.root = tk.Tk()
@@ -23,7 +34,7 @@ class applacation():
 
         # 标题
         tk.Label(self.root, text='Tiri', font=('宋体', 30)).pack()
-        tk.Label(self.root, text='您的人工智障问答软件 V1.0.4', font=('宋体', 12)).pack()
+        tk.Label(self.root, text='您的人工智障问答软件 V1.0.4 I', font=('宋体', 12)).pack()
 
         # mainFrame
         self.mainFrame = tk.LabelFrame(self.root, text='输入框')
@@ -49,14 +60,14 @@ class applacation():
         self.mainSendText.pack(side=tk.LEFT)
         self.mainSendText.bind("<Return>", self.send)
 
-        self.mainSendButton = ttk.Button(self.mainSendFrame, text='发送', width=10, command=self.send)
+        self.mainSendButton = ttk.Button(self.mainSendFrame, text='发送', command=self.send)
         self.mainSendButton.pack(side=tk.RIGHT)
 
         # mainFrame pack
         self.mainFrame.pack()
 
         # 帮助按钮
-        self.helpButton = ttk.Button(self.root, text='帮助', width=10, command=self.help)
+        self.helpButton = ttk.Button(self.root, text='帮助', command=self.help)
         self.helpButton.pack()
 
         # mainloop
